abc9f7000e78d35930a998f705dfdc67afb643b4
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-04-26 17:23:56 +0300

    The bare-bones structure of the admin-related pages is now set up.

---
+++ src/pages/admin/AdminLocationsPage.jsx
@@ -0,0 +1,5 @@
+const AdminLocationsPage = () => {
+  return;
+};
+
+export default AdminLocationsPage;
---
+++ src/pages/admin/AdminReservationsPage.jsx
@@ -0,0 +1,5 @@
+const AdminReservationsPage = () => {
+  return;
+};
+
+export default AdminReservationsPage;
