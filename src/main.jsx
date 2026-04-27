10b0246e686e8632799003093534ae1273b6f6e0
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-04-27 23:04:38 +0300

    Tried to set up routing to no avail.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// Rendering Related Imports
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+// React-Router-DOM Related Imports
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Public Page Imports
+import HomePage from "./pages/HomePage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import LoginPage from "./pages/auth/LoginPage.jsx";
+import RegisterPage from "./pages/auth/RegisterPage.jsx";
+import RoomSearchPage from "./pages/RoomSearchPage.jsx";
+
+// Global Styling Import
+import "./index.css";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "*", element: <NotFoundPage /> },
+  { path: "/about", element: <AboutPage /> },
+  { path: "/login", element: <LoginPage /> },
+  { path: "/register", element: <RegisterPage /> },
+  { path: "/rooms", element: <RoomSearchPage />, children: [] },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
