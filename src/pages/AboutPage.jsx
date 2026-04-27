93ff4efc82b4ed5ae4f98547a3e33138f036544c
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-04-27 23:07:58 +0300

    Tried to expand upon the About page, but it didn't work as expected, because something is preventing everything from rendering.

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,15 @@
+import { Box, Typography } from "@mui/material";
+import NavBar from "../components/NavBar.jsx";
+
 const AboutPage = () => {
-  return;
+  return (
+    <>
+      <Box>
+        <NavBar />
+        <Typography variant="h3">About</Typography>
+      </Box>
+    </>
+  );
 };
 
 export default AboutPage;
