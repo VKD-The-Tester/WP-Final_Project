ac07e31669312e76162e612aa603bbe5f7c6249c
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-04-27 23:06:32 +0300

    Tried to create the pages  for the requirements but I gave up at the end.

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,9 +1,62 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { Box, Typography, TextField, Button } from "@mui/material";
+import { BACKEND_CLIENT } from "../../api/axios.js";
+import { setCookie } from "react-cookie";
 
 const LoginPage = () => {
   const [form, setForm] = useState({ email: "", password: "" });
-  return;
+
+  const loginExistingUser = () => {
+    const response = BACKEND_CLIENT.post("api/auth/login", {
+      email: form.email,
+      password: form.password,
+    });
+
+    const token = response.data.token;
+    const tokenData = JSON.parse(atob(token.split(".")[1]));
+    const expirationDate = new Date(tokenData.exp);
+
+    setCookie("token", token, { expires: expirationDate });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        maxWidth: "20em",
+        m: "2em",
+        gap: "1em",
+      }}
+    >
+      <Typography variant="h4">Login</Typography>
+      <TextField
+        label="Email"
+        variant="outlined"
+        fullwidth
+        value={form.email}
+        onChange={(e) => setForm({ ...form, email: e.target.value })}
+      ></TextField>
+      <TextField
+        label="password"
+        type="password"
+        variant="outlined"
+        fullwidth
+        value={form.password}
+        onChange={(e) => setForm({ ...form, password: e.target.value })}
+      ></TextField>
+      <Button component={NavLink} to="" variant="contained" fullwidth>
+        Login
+      </Button>
+    </Box>
+  );
 };
 
 export default LoginPage;
--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -1,7 +1,73 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { Box, Typography, Button, TextField } from "@mui/material";
+import { BACKEND_CLIENT } from "../../api/axios.js";
+import * as Yup from "yup";
 
 const RegisterPage = () => {
   const [form, setForm] = useState({ name: "", email: "", password: "" });
-  return;
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Name field is required"),
+    email: Yup.string()
+      .email("Email format is invalid")
+      .required("Email field is required"),
+    password: Yup.string().required("Password field is required"),
+  });
+
+  const registerNewUser = async () => {
+    await BACKEND_CLIENT.post("/api/auth/register", {
+      name: form.name,
+      email: form.email,
+      password: form.password,
+    });
+  };
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        maxWidth: "20em",
+        m: "2em",
+        gap: "1em",
+      }}
+    >
+      <Typography variant="h4">Register</Typography>
+      <TextField
+        variant="outlined"
+        fullwidth
+        label="Name"
+        value={form.name}
+        onChange={(e) => setForm({ ...form, name: e.target.value })}
+      ></TextField>
+      <TextField
+        variant="outlined"
+        fullwidth
+        label="Email"
+        value={form.email}
+        onChange={(e) => setForm({ ...form, email: e.target.value })}
+      ></TextField>
+      <TextField
+        variant="outlined"
+        fullwidth
+        label="password"
+        type="password"
+        value={form.password}
+        onChange={(e) => setForm({ ...form, password: e.target.value })}
+      ></TextField>
+      <Button
+        component={NavLink}
+        to="/login"
+        variant="contained"
+        fullwidth
+        onClick={() => registerNewUser()}
+      >
+        Register
+      </Button>
+    </Box>
+  );
 };
+
+export default RegisterPage;
